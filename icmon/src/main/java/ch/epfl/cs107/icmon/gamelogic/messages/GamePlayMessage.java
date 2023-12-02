1e344f0fb4ecef908aa5f38513b16303d2e8b703
balthaB <balthazar.baillat@epfl.ch>
2023-12-02 16:14:07 +0100

    messages-dev

---
+++ icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/messages/GamePlayMessage.java
@@ -0,0 +1,5 @@
+package ch.epfl.cs107.icmon.gamelogic.messages;
+
+public interface GamePlayMessage {
+    void process();
+}
---
+++ icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/messages/PassDoorMessage.java
@@ -0,0 +1,14 @@
+package ch.epfl.cs107.icmon.gamelogic.messages;
+
+import ch.epfl.cs107.icmon.ICMon;
+
+public class PassDoorMessage implements GamePlayMessage{
+    private String message;
+
+    public PassDoorMessage () {}
+
+    @Override
+    public void process() {
+
+    }
+}
