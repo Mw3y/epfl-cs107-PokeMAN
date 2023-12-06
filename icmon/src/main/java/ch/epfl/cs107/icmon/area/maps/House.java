752063bb477005b391bdea0a7532172dbfd8eb98
balthaB <balthazar.baillat@epfl.ch>
2023-12-06 15:00:58 +0100

    WIP

--- icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/House.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/House.java
@@ -1,10 +1,12 @@
 package ch.epfl.cs107.icmon.area.maps;
 
-import ch.epfl.cs107.play.areagame.area.Area;
+import ch.epfl.cs107.icmon.actor.misc.Door;
+import ch.epfl.cs107.icmon.area.ICMonArea;
 import ch.epfl.cs107.play.engine.actor.Background;
 import ch.epfl.cs107.play.engine.actor.Foreground;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
-public class House extends Area {
+public class House extends ICMonArea {
 
     public static final String TITLE = "house";
 
@@ -14,12 +16,14 @@ public class House extends Area {
     }
 
     @Override
-    public float getCameraScaleFactor() {
-        return 0;
+    public DiscreteCoordinates getPlayerSpawnPosition() {
+        return new DiscreteCoordinates(2, 2);
     }
 
     public void createArea() {
         registerActor(new Background(this));
         registerActor(new Foreground(this));
+        registerActor(new Door(House.TITLE, new DiscreteCoordinates(7, 26), this,
+                new DiscreteCoordinates(3, 1), new DiscreteCoordinates(4, 1)));
     }
 }
